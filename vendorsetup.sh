86dca801cb7f1f2d83f48d65f516184990af6192
TH779 <i@779.moe>
2021-10-29 13:16:31 +0800

    violet: Commonize IFAAManager
    * Boot jars can only be used in the root namespace.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I605593ad59c755b65d8a4f4358b3531d97e792bd
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/display
+rm -rf device/xiaomi/extras
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/Evolution-X-Devices/device_xiaomi_extras.git device/xiaomi/extras
