2efff8de1113f7535aec36c279d932844d63958f
princejb786 <princejb.jin@gmail.com>
2021-11-03 12:20:38 +0530

    violet: Add evolution_security in vendorsetup.sh The repo could be found here:- https://github.com/Evolution-X/evolution_security
    Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,4 @@ git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_audio.gi
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
 git clone https://github.com/Evolution-X-Devices/device_xiaomi_extras.git device/xiaomi/extras
+git clone https://github.com/Evolution-X/evolution_security.git vendor/evolution/build/target/product/security
