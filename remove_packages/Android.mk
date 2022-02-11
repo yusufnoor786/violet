Evolution-X-Devices/device_xiaomi_violet

[SQUASH]violet: Remove packages at build time
Squash of the following:

violet: remove_packages: Rename RemovePackages for source

violet: remove_packages: Switch to platform certificate

Co-authored-by: naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
Co-authored-by: Arturo <rtsanonynousperson@gmail.com>
Signed-off-by: princejb786 <sam.jbjin@gmail.com>

remove_packages/Android.mk
@@ -1,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePkgs
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Ornament SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger WallpapersBReel2020 arcore obdm_stub DevicePolicyPrebuilt RecorderPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)

yusufnoor786/violet

Signed-off-by: Mir Yasin Ali <miryasin@m.ms.evolution-x.org>

remove_packages/Android.mk
@@ -1,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePkgs
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Drive ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt RecorderPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)