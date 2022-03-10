df0ea2269f776bb212bc6f4e554792711bebde2e
Joker-V2 <nipinna0@gmail.com>
2022-03-10 18:50:58 +0000

    violet: De-commonize IFAAManager
    Change-Id: Ic96355a522ed1a9c3a942be4b4892eab837fbf19

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,9 @@
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/display
-rm -rf device/xiaomi/extras
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
 git clone --depth=1 https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
-git clone https://github.com/Evolution-X-Devices/device_xiaomi_extras.git device/xiaomi/extras
 git clone https://github.com/Evolution-X/evolution_security.git vendor/evolution/build/target/product/security
 git clone --depth=1 https://github.com/EvolutionXViolet/vendor_xiaomi_violet.git vendor/xiaomi/violet
 git clone --depth=1 https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git kernel/xiaomi/violet
