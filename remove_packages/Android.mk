7a6ab600ef18cb8070326426f4503cdf7b668817
Mir Yasin Ali <miryasin@m.ms.evolution-x.org>
2022-03-19 15:32:57 +0530

    violet: remove_packages: Update some packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,11 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Drive ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt PrebuiltGmail AppDirectedSMSService Drive ConnMO DCMO
 LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += Photos PrebuiltBugle SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
 LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += PixelWallpapers2020  VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
